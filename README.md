64f48023014d1dc287049c460d4ce42d0a22d3ae
theskyspace <akashrockzz411@gmail.com>
2023-01-30 14:54:24 -0500

    Postgresql setup

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# backend
+### How to setup in your Ubuntu machine. :technologist:
+
+[Testing url]()\
+[Production url]()
+
+---
+> **_NOTE: All the cmds must be executed on terminal_** 
+
+1. Postgresql Setup:
+  ```python
+  sudo -u postgres psql
+  CREATE DATABASE dream;
+  CREATE USER dream WITH PASSWORD 'dream';
+  ALTER ROLE dream SET client_encoding TO 'utf8';
+  ALTER ROLE dream SET default_transaction_isolation TO 'read committed';
+  ALTER ROLE dream SET timezone TO 'UTC';
+  GRANT ALL PRIVILEGES ON DATABASE dream TO dream;
+  ```
+ 2. Python setup:
+ ```python
+ pip install -r requirements.txt
+ sudo systemctl start postgresql
+ ```
+ 
+---
+ 
+
+
+Cheers
+Happy Coding
--- core/settings.py
+++ core/settings.py
@@ -9,11 +9,6 @@ env = environ.Env(
 
 environ.Env.read_env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -99,16 +94,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DREAM_DB_NAME'),
+        'USER': env('DREAM_DB_USER'),
+        'PASSWORD': env('DREAM_DB_PASSWORD'),
+        'HOST': env('DREAM_DB_HOST'),
+        'PORT': env('DREAM_DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -154,3 +151,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
